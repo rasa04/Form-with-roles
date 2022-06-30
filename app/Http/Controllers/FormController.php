d92d0e77da4e1a3c74c4108ee18716adf412a64b
Rasa <abdurasul2004.02@gmail.com>
2022-06-30 14:23:29 +0500

    user can login and send form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\StoreRequest;
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function form(){
         return view('form');
+    }
+    public function store(StoreRequest $request){
+        $data = $request->validated();
+        Application::create($data);
+        return "Отправлено!";
     }
 }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Форма</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+            {{ __('You are logged in!') }}
+        </div>
+        <form action="{{ route('form.create') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="text" placeholder="Topic" name="topic">
+            <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+            <input type="hidden" name="status" value="0">
+            <textarea name="message" id="message" placeholder="message"></textarea>
+            <input type="file" name="file" id="file">
+            <button type="submit">SEND</button>
+
+            {{ $errors }}
+        </form>
+    </div>
+@endsection
